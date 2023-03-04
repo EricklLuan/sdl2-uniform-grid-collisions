6b16338e11b19bd35a1310af8d35d5b1df1f763b
Erick Luan <erick.luan.silva1308@gmail.com>
2023-03-04 15:47:27 -0300

    remaking again

--- src/game.cpp
+++ src/game.cpp
@@ -19,19 +19,6 @@ Game::~Game() {
 void Game::run() {
   if (!init) return;
 
-  UniformGrid uniform_grid = UniformGrid(Vector2(10, 10), window.vSize);
-
-  std::vector<Object> objects = {
-    Object{Vector2(rand() % 100, rand() % 100), Vector2(20, 20)},
-    Object{Vector2(rand() % 100, rand() % 100), Vector2(20, 20)},
-    Object{Vector2(rand() % 100, rand() % 100), Vector2(20, 20)},
-    Object{Vector2(rand() % 100, rand() % 100), Vector2(20, 20)},
-  };
-
-  for (Object &a: objects) {
-    uniform_grid.add(a);
-  }
-
   while (true) {
     Uint32 startTimer = SDL_GetTicks();
 
@@ -41,32 +28,12 @@ void Game::run() {
         return;
         break;
       case SDL_KEYDOWN:
-        if (e.key.repeat == 0) {
-          if (e.key.keysym.scancode == SDL_SCANCODE_D) objects[0].position.x += 1;
-          if (e.key.keysym.scancode == SDL_SCANCODE_A) objects[0].position.x -= 1;
-          if (e.key.keysym.scancode == SDL_SCANCODE_W) objects[0].position.y -= 1;
-          if (e.key.keysym.scancode == SDL_SCANCODE_S) objects[0].position.y += 1;
-        }
+        break;
       }
     }
 
     window.clear();
    
-    for (int i = 0; i < objects.size(); i++) {
-      SDL_Rect rect = {
-        (int)objects[i].position.x,
-        (int)objects[i].position.y,
-        (int)objects[i].size.x,
-        (int)objects[i].size.y,
-      };
-
-      SDL_SetRenderDrawColor(window.getRenderer(), 0, 0, 0, 255);
-      SDL_RenderDrawRect(window.getRenderer(), &rect);
-
-      uniform_grid.update(objects[i].id);
-    }
-    uniform_grid.checkCollision(objects[0].id);
-
     window.flip();
 
     int frameTicks = SDL_GetTicks() - startTimer;
